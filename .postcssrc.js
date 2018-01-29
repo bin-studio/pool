76dcbc91d370e6ed4bdd91524e0ef6768ba1785b
N Kort <nics@limnerstudio.co.uk>
2018-01-29 18:22:19 +0200

    add postcss settings and files

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,14 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
+    "precss": {},
+    "postcss-custom-properties": {},
+    "postcss-custom-media": {},
+    "postcss-calc": {},
+    "postcss-nested": {},
+    "postcss-color-function": {},
+    "postcss-discard-comments": {},
+    "postcss-reporter": {},
     "autoprefixer": {}
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'style/imports';
+  @import 'style/global';
 </style>
