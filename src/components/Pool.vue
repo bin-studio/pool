06693801c64bfed328b61d48013ac0a8d9ad1d44
N Kort <nics@limnerstudio.co.uk>
2018-01-31 13:04:33 +0200

    update pool fetch

--- src/components/Pool.vue
+++ src/components/Pool.vue
@@ -1,27 +1,28 @@
 <template>
   <section class="page p4">
     <thumb :pool="pool"></thumb>
+    <!-- <upload :contract="pool" image-type="heroImage"></upload> -->
   </section>
 </template>
 
 <script>
-import Thumb from './PoolThumb'
+import store from '@/store'
+import Thumb from '@/components/PoolThumb'
+import Upload from '@/components/Upload'
+
 export default {
   name: 'Pool',
-  props: ['address'],
-  components: {
-    Thumb
-  },
   computed: {
     pool () {
       return this.$store.state.pool
     }
   },
-  created () {
-    this.$store.dispatch('getPoolDb', this.address)
-  }
+  beforeRouteEnter (to, from, next) {
+    const { address } = to.params
+    store.dispatch('getPoolDb', address).then(() => {
+      next()
+    })
+  },
+  components: { Thumb, Upload }
 }
 </script>
-
-<style lang="css" scoped>
-</style>
