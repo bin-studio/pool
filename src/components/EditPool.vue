3901f2697532d1c41620439bf897f255fdcad33e
evvvritt <evv.info@gmail.com>
2018-01-31 16:38:16 +0200

    editpool - upload image, saving gets 401 for now

--- src/components/EditPool.vue
+++ src/components/EditPool.vue
@@ -1,15 +1,29 @@
 <template>
   <article class="page p4">
     <div class="max-width-4 mx-auto border">
-      <header class="border-bottom bold clearfix">
+      <header class="bold clearfix">
         <div class="col col-4 px2 py1">{{pool.symbol}}&nbsp;</div>
         <div class="col col-4 px2 py1 border-left">{{pool.type}}&nbsp;</div>
         <div class="col col-4 px2 py1 border-left">{{pool.base}}&nbsp;</div>
       </header>
-      <section class="px2 py3">
-        <div><input type="text" v-model="newPool.name"></div>
-        <textarea class="block mt2" rows="4" v-model="newPool.description"></textarea>
+      <section class="px2 py3 border-top">
+        <div>
+          <input type="text" v-model="newPool.name">
+        </div>
+        <div>
+          <textarea class="block mt2" rows="3" v-model="newPool.description"></textarea>
+        </div>
       </section>
+      <section class="px2 py3 border-top">
+        <upload :contract="newPool" image-type="heroImage"></upload>
+        <pool-image :src="newPool.heroImage" :bg="false" size="hero" class="block mx-auto"></pool-image>
+      </section>
+      <footer>
+        <div class="clearfix border-top">
+          <button class="btn block col col-6" @click="$router.go(-1)">Cancel</button>
+          <button class="btn block col col-6 bg-blue white" @click="submit">Save</button>
+        </div>
+      </footer>
     </div>
   </article>
 </template>
@@ -17,6 +31,7 @@
 <script>
 import store from '@/store'
 import Upload from '@/components/Upload'
+import PoolImage from '@/components/Image'
 
 export default {
   name: 'EditPool',
@@ -30,18 +45,29 @@ export default {
       return this.$store.state.pool
     }
   },
-  watch: {
-    pool (val) {
-      this.newPool = JSON.parse(JSON.stringify(val))
-    }
-  },
   beforeRouteEnter (to, from, next) {
     const { address } = to.params
     store.dispatch('getPoolDb', address).then(() => {
       next()
     })
   },
-  components: { Upload }
+  watch: {
+    pool () {
+      this.clonePool()
+    }
+  },
+  methods: {
+    clonePool () {
+      this.newPool = JSON.parse(JSON.stringify(this.pool))
+    },
+    submit () {
+      this.$store.dispatch('updateContract', this.newPool)
+    }
+  },
+  created () {
+    this.clonePool()
+  },
+  components: { Upload, PoolImage }
 }
 </script>
 
