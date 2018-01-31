64304ee0d421e6c259cf2b4d3b8b0809dc29aaf8
evvvritt <evv.info@gmail.com>
2018-01-31 12:47:30 +0200

    Graph > Trade: add buy/sell actions, extra row

--- src/components/PoolThumb.vue
+++ src/components/PoolThumb.vue
@@ -3,25 +3,25 @@
     <section class="border" :class="{'rounded-bottom': !join, 'pool--collapsed': join}">
       <header class="flex justify-between items-stretch border-bottom">
         <div class="col col-9 px2 flex items-center"><span>Jump in the Pool</span></div>
-        <div class="col col-3 flex items-stretch bg-dots border-left" @click="graph = !graph">
-          <popout :pop="graph" class="col-12 flex items-center justify-center pointer">
-            <span>Graph</span>
+        <div class="col col-3 flex items-stretch bg-dots border-left" @click="trade = !trade">
+          <popout :pop="trade" class="col-12 flex items-center justify-center pointer">
+            <span>Trade</span>
           </popout>
         </div>
       </header>
       <figure class="bg-dots relative">
         <div class="absolute top-0 right-0 bottom-0 left-0 overflow-hidden" @click="join = false">
-          <pool-image v-show="!graph" :bg="true" :src="pool.heroImage" class="absolute top-0 left-0 col-12"></pool-image>
+          <pool-image v-show="!trade" :bg="true" :src="pool.heroImage" class="absolute top-0 left-0 col-12"></pool-image>
         </div>
-        <popout v-show="graph" :pop="graph" class="absolute-fill bg-white">
-          <graph class="absolute-fill"></graph>
+        <popout v-show="trade" :pop="trade" class="bg-white">
+          <trade></trade>
         </popout>
       </figure>
       <!-- text -->
       <footer class="center border-top">
         <div class="p3">
           <router-link :to="{name: 'Pool', params: {address: pool.address}}">
-            <h1 class="bold">{{ pool.name }}</h1>
+            <h1 class="h4 bold">{{ pool.name }}</h1>
           </router-link>
           <div class="mt1" v-html="pool.description"></div>
         </div>
@@ -38,7 +38,7 @@
 
 <script>
 import Popout from '@/components/Popout'
-import Graph from './PoolGraph'
+import Trade from './PoolTrade'
 import Join from './PoolJoin'
 import PoolImage from '@/components/Image'
 
@@ -49,19 +49,19 @@ export default {
   },
   data () {
     return {
-      graph: false,
+      trade: false,
       join: false
     }
   },
   watch: {
     join () {
-      if (this.join) this.graph = false
+      if (this.join) this.trade = false
     },
-    graph () {
-      if (this.graph) this.join = false
+    trade () {
+      if (this.trade) this.join = false
     }
   },
-  components: { Popout, Graph, Join, PoolImage }
+  components: { Popout, Trade, Join, PoolImage }
 }
 </script>
 
@@ -70,7 +70,7 @@ header{
   height:3rem;
 }
 figure{
-  padding-bottom:16em;
+  min-height:16em;
   .bg-img-cover{
     height:100%;
   }
