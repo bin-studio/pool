f63c187951784df2b2c0487bd8e4c379cc0300c3
evvvritt <evv.info@gmail.com>
2018-02-01 02:34:33 +0200

    pool thmb - img = dims

--- src/components/PoolThumb.vue
+++ src/components/PoolThumb.vue
@@ -1,6 +1,6 @@
 <template>
   <article class="mb4">
-    <section class="border" :class="{'rounded-bottom': !join, 'pool--collapsed': join}">
+    <section class="border" :class="{'rounded-bottom': !join}">
       <header class="flex justify-between items-stretch border-bottom">
         <div class="col col-6 px2 flex items-center bold uppercase"><span>{{pool.symbol}}</span></div>
         <!-- edit button -->
@@ -15,7 +15,7 @@
         </div>
       </header>
       <!-- image / trade view -->
-      <figure class="bg-dots relative">
+      <figure class="bg-dots relative" :class="{'pool__figure--collapsed': join, 'pool__figure--trade': trade}">
         <div class="absolute top-0 right-0 bottom-0 left-0 overflow-hidden" @click="join = false">
           <pool-image v-show="!trade" :bg="true" :src="pool.heroImage" class="absolute top-0 left-0 col-12"></pool-image>
         </div>
@@ -98,19 +98,21 @@ header{
   height:3rem;
 }
 figure{
-  min-height:16em;
+  padding-bottom:calc(800/1280 * 100%);
   .bg-img-cover{
     height:100%;
   }
-  .pool--collapsed &{
-    min-height:0;
-    height:5.75rem;
+  &.pool__figure--collapsed{
+    padding-bottom:5.75rem;
     & .bg-img-cover{
       filter:blur(4px);
       height:120%;
       width:104%;
       transform:translateX(-2%) translateY(-10%);
     }
+  }
+  &.pool__figure--trade{
+    padding-bottom:0;
   }
 }
 .pool__join{
